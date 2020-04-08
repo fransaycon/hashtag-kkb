92e0430aab0155f14b1b5e5aa839eeb3332c8a99
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2020-04-08 16:37:41 +0800

    Create kkb-start styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { KkbStartComponent } from './kkb-start/kkb-start.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KkbStartComponent } from './kkb-start/kkb-start.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, KkbStartComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
