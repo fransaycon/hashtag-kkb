8231c715ad0a9c6a1bff252f2f23e5c1142b1695
Franrey Anthony S. Saycon <franreysaycon@gmail.com>
2020-04-16 21:43:39 +0800

    Remove stray imports

--- src/app/dashboard/cart/cart.component.ts
+++ src/app/dashboard/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import User from 'src/app/services/users/user.model';
 import { Subscription } from 'rxjs';
